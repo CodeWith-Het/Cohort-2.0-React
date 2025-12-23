6ef781f861cf586365592948a38214addafdbb92
CodeWith-Het <hetp1065@gmail.com>
2025-12-23 07:47:33 -0600

    Deploy react class 4 task done 23/12/2025
    Task done

--- class3-task/vite.config.js
+++ class3-task/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/Cohort-2.0-React/class3-task/",
 })
