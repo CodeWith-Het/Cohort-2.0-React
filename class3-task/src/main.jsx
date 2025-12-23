20e6c44d565ae56f4c4ecac32ac6b3d6c4474351
CodeWith-Het <hetp1065@gmail.com>
2025-12-23 08:20:59 -0600

    done

--- class3-task/src/main.jsx
+++ class3-task/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Cohort-2.0-React/class3-task">
+    <BrowserRouter basename="/Cohort-2.0-React/class3-task/">
       <App />
     </BrowserRouter>
   </StrictMode>,
