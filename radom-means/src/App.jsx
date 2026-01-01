6e9b569463743ea79cb21b2963299f12232cc1ba
CodeWith-Het <hetp1065@gmail.com>
2026-01-01 06:54:09 -0600

    Class11-task axios mems gernated

--- radom-means/src/App.jsx
+++ radom-means/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import axios from "axios"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+const [mems, setMems] = useState(null)
+
+const getMemsData = async () =>{
+  const answer = await axios.get("https://api.imgflip.com/get_memes")
+
+  const merge = answer.data.data.memes
+
+  const r1 = Math.floor(Math.random()*(answer.data.data.memes.length))
+  setMems(merge[r1])
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen flex flex-col items-center justify-center bg-red-800 gap-5'>
+
+      <h1 className='text-2xl text-white font-bold'>😂 Mems gernator</h1>
+      <img src={mems?.url} alt="" className='h-72 w-62 object-cover ' />
+
+        <button 
+        onClick={()=>getMemsData()}
+        className='px-4 py-2 bg-green-600 text-white rounded-3xl border-0 hover:bg-green-700 active:scale-95 cursor-pointer'>New Mems Gernated</button>
+    </div>
   )
 }
 
 export default App
+
+// https://api.imgflip.com/get_memes   this is api
